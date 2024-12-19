6d0bd12528a1938e4a3a1839e1110c771e22e89f
IulianBotnari <iulianbotnari93@gmail.com>
2024-12-19 09:45:10 +0100

    Add registration route

--- backend/server.js
+++ backend/server.js
@@ -34,21 +34,76 @@ db.query('SELECT * FROM users', (err, result) => {
 
 
 
-console.log(users.map(user => user.username));
 
-passport.use(expressSession({
-    secret: secret_key,
-    resave: false,
-    saveUninitialized: false,
-}))
+
+passport.use(new LocalStrategy(
+    async (username, password, done) => {
+        try {
+            db.query('SELECT * from users WHERE username = ?', [username], async (err, result) => {
+
+                if (err) throw done(err)
+
+                const user = result[0] // da capire perchè --> result[0] e non result e basta <---
+                if (!user) return done(null, false, { message: 'Incorrect user' })
+
+
+                const isMatch = await bcrypt.compare(password, user.password) // ⬅️ possibile bug, oppure e da capire meeglio questa riga
+                if (!isMatch) return done(null, false, { message: 'Incorrect password' })
+
+                return done(null, user)
+            })
+        } catch (err) { return done(err) }
+    }))
+
+
+// Serializzazione del utente 
+passport.use.serializeUser((user, done) => done(null, user.id))
+
+
+// Deserializzazione del utente
+passport.deserializeUser((id, done) => {
+    db.query('SELECT * FROM users WHERE id =?', [id], (err, result) => {
+        if (err) throw err
+        done(null, result[0])
+    })
+})
+// rotta per registrare l'utente
+server.post('/register', async (req, res) => {
+    const { username, password, email } = req.body;
+
+    try {
+        // Verifica se l'utente esiste già
+        db.query('SELECT * FROM users WHERE username = ?', [username], async (err, results) => {
+            if (err) return res.status(500).json({ message: 'Errore interno del server' })
+
+            if (results.length > 0) {
+                return res.status(400).json({ message: 'Username già esistente' })
+            }
+
+            // Hashing della password
+            const hashedPassword = await bcrypt.hash(password, 10)
+
+            // Inserisci il nuovo utente nel database
+            db.query('INSERT INTO users (username, password, email) VALUES (?, ?, ?)', [username, hashedPassword, email], (err, results) => {
+                if (err) return res.status(500).json({ message: 'Errore nella creazione dell\'utente' });
+                res.status(201).json({ message: 'Utente creato con successo' });
+            });
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Errore durante la registrazione' })
+    }
+});
+
+
+
+
 
 
 
 server.get('/', router)
 
-server.get('/test', (req, res) => {
-    res.send('Welcome to the RESTful API')
-})
+
+
 
 server.listen(port, () => {
     console.log(`Server is running at http://${host}:${port}`)
