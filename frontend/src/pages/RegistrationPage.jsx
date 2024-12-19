20ae0d7618a0af1bfc3d205f951129d9f0d734f2
IulianBotnari <iulianbotnari93@gmail.com>
2024-12-19 11:42:34 +0100

    Registration form Work!! Hashed pass ok!

--- frontend/src/pages/RegistrationPage.jsx
+++ frontend/src/pages/RegistrationPage.jsx
@@ -1,25 +1,40 @@
-
+import { useState } from 'react'
 
 export default function RegistrationPage() {
+    const [email, setEmail] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
 
+    const handleRegistration = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await fetch('http://127.0.0.1:3000/register', {
+                method: 'POST',
+                headers: { 'content-type': 'application/json' },
+                body: JSON.stringify({ email, username, password })
+            })
+        } catch (error) {
+            alert(error.message)
+        }
+    }
 
     return (
         <>
-            <form>
+            <form onSubmit={handleRegistration}>
                 <div className="mb-3">
                     <label forhtml="InputEmail" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="InputEmail" aria-describedby="emailHelp" />
+                    <input type="email" className="form-control" id="InputEmail" aria-describedby="emailHelp" onChange={(e) => setEmail(e.target.value)} />
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
                     <label forhtml="InputUser" className="form-label">Username</label>
-                    <input type="text" className="form-control" id="InputUser" aria-describedby="textHelp" />
+                    <input type="text" className="form-control" id="InputUser" aria-describedby="textHelp" onChange={(e) => setUsername(e.target.value)} />
                     <div id="emailHelp" className="form-text">We'll never share your user with anyone else.</div>
                 </div>
 
                 <div className="mb-3">
                     <label forhtml="InputPassword" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="InputPassword" />
+                    <input type="password" className="form-control" id="InputPassword" onChange={(e) => setPassword(e.target.value)} />
                 </div>
 
                 <button type="submit" className="btn btn-primary">Submit</button>
