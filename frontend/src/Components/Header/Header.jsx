9a01e7ffa912ad0a97e7c7819499dadb39819cc3
IulianBotnari <iulianbotnari93@gmail.com>
2024-12-24 17:07:10 +0100

    Completed Header Style

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,72 +1,71 @@
 
 import { Outlet, NavLink, Navigate } from 'react-router'
 import { useGlobalContext } from '../../GlobalContext/GlobalContext.jsx';
+
+import { useState } from "react";
 import Style from '../Header/Header.module.css'
 
 
 export default function Header() {
+    const [activeIndex, setActiveIndex] = useState(-1)
 
-    const { setLogged } = useGlobalContext()
 
-    const handleLogout = () => {
-        // Rimuovi il token dal localStorage
-        localStorage.removeItem('token');
-        setLogged('false')
-
+    const handleNavClick = (index) => {
+        setActiveIndex(index - 1)
     };
-
 
     return (
         <>
             <header className={Style.header}>
-                <navigator className={Style.navigation}>
-
+                <nav className={Style.navigation}>
                     <ul className={Style.ul}>
-                        <li className={Style.li} >
-                            <NavLink to='/home' className=''>
+                        <li className={Style.li} onClick={() => handleNavClick(0)}>
+                            <NavLink
+                                to='/home'
+                                className={({ isActive }) => isActive ? `${Style.active1} ${Style.a} ` : Style.a}
+                            >
                                 <span className={Style.icon}>
                                     <i className='bi-house'></i>
                                 </span>
-                                <span className='text'>Home</span></NavLink>
-
-
+                                <span className={Style.text}>Home</span>
+                            </NavLink>
                         </li>
 
-                        <li className='active'>
-                            <NavLink to='/login' className=''>
+                        <li className={Style.li} onClick={() => handleNavClick(1)}>
+                            <NavLink
+                                to='/login'
+                                className={({ isActive }) => isActive ? `${Style.a} ${Style.active1}` : Style.a}
+                            >
                                 <span className={Style.icon}>
                                     <i className='bi-door-open'></i>
                                 </span>
-                                <span className='text'>Login</span>
+                                <span className={Style.text}>Login</span>
                             </NavLink>
                         </li>
 
-
-                        <li className={Style.li} >
-
-                            <NavLink to='/signin' className=''>
+                        <li className={Style.li} onClick={() => handleNavClick(2)}>
+                            <NavLink
+                                to='/signin'
+                                className={({ isActive }) => isActive ? `${Style.a} ${Style.active1}` : Style.a}
+                            >
                                 <span className={Style.icon}>
                                     <i className='bi-box-arrow-in-right'></i>
                                 </span>
-                                <span className='text'>Signin</span>
+                                <span className={Style.text}>Signin</span>
                             </NavLink>
-
                         </li>
 
-                        <div className='indicator'></div>
 
-
+                        <div
+                            className={Style.indicator}
+                            style={{ transform: `translateX(${activeIndex * 700}%)` }}
+                        />
                     </ul>
-
-
-                </navigator>
-
-
-                {/* <button onClick={handleLogout} className='btn btn-primary'> Logout</button> */}
-            </header >
-
-
+                </nav>
+            </header>
         </>
-
-    )
+    );
 }
+
+
+
